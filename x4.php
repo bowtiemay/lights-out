6ac857f817eb51baf01b8311b57b8c6469b92041
maycode <62029215+mayacobb@users.noreply.github.com>
2021-08-15 00:21:33 -0700

    Create x4.php

---
+++ x4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lights Out!</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="x4.css" />
+    <style>
+    </style>
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.js"
+      integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+      crossorigin="anonymous"></script>
+      <script src="x4.js"></script>
+</head>
+<body>
+  <h1>X4: Lights Out</h1>
+<table>
+    <tr><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td></tr>
+</table>
+<h2>YOU WON!</h2>
+<br>
+<button type="button" id="reset">Reset Game</button>
+</body>
+</html>
